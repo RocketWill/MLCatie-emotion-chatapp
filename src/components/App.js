69635a725da0f696c61aa11a4bc42cb02691f9c0
Yong Cheng <will.chengyong@gmail.com>
2019-05-28 11:23:13 +0800

    Completed pass user data to user panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+import {Grid, Message} from 'semantic-ui-react';
+import "./App.css";
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+import { connect } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
+
+const App = ({currentUser}) => (
+  <Grid columns="equal" className="app" style={{background: "#eee"}}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+    
+  </Grid>
+)
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+const mapDispatchToProps = {
+  
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import firebase from '../../firebase';
+import { auth } from 'firebase';
+import { connect } from 'react-redux';
+
+class UserPanel extends React.Component {
+    state = {
+        user: this.props.currentUser
+    }
+
+
+    dropDownOptions = () => [
+        {   
+            key: 'user',
+            text: <span>以 <strong>{this.state.user.displayName}</strong> 身份登入</span>,
+            disable: true
+        },
+        {
+            key: 'avatar',
+            text: <span>更换头像</span>
+        },
+        {
+            key: 'signout',
+            text: <span onClick={this.handleSignout}>登出</span>
+        }
+    ];
+
+    handleSignout = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => console.log("signed out"))
+    }
+
+    render(){
+        const {user} = this.state;
+        return (
+            <Grid style={{background: '#4c3c4c'}}>
+                <Grid.Column>
+                    <Grid.Row style={{padding: '1.2em', margin: 0}}>
+                        {/* App Header */}
+                        <Header inverted floated="left" as="h3">
+                            <Icon name="code" />
+                            <Header.Content>喵喵机器喵</Header.Content>
+                        </Header>
+                        {/* User Dropdown */}
+                    <Header style={{padding: '0.25em'}} as="h4" inverted>
+                        <Dropdown trigger={
+                            <span>
+                                <Image src={user.photoURL} spaced="right" avatar />
+                                {user.displayName}
+                            </span>
+                        } options={this.dropDownOptions()} />
+                    </Header>
+                    </Grid.Row>
+
+                    
+                </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default UserPanel;
