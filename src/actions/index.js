17456e0e417dc48c38555b2192783f849bbad4c6
Yong Cheng <will.chengyong@gmail.com>
2019-06-03 16:48:49 +0800

    Completed change theme color

--- src/actions/index.js
+++ src/actions/index.js
@@ -44,3 +44,15 @@ export const setUserPosts = (userPosts) => {
     }
 }
 
+// color actions
+export const setColors = (primaryColor, secondaryColor) => {
+    return {
+        type: actionTypes.SET_COLORS,
+        payload: {
+            primaryColor,
+            secondaryColor
+        }
+    }
+}
+
+
--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,16 @@ import MetaPanel from './MetaPanel/MetaPanel';
 import { connect } from 'react-redux';
 
 
-const App = ({currentUser, currentChannel, isPrivateChannel, userPosts}) => (
-  <Grid columns="equal" className="app" style={{background: "#eee"}}>
-    <ColorPanel />
+const App = ({currentUser, currentChannel, isPrivateChannel, userPosts, primaryColor, secondaryColor}) => (
+  <Grid columns="equal" className="app" style={{background: secondaryColor}}>
+    <ColorPanel 
+      key={currentUser && currentUser.name}
+      currentUser={currentUser}
+    />
     <SidePanel 
       key={currentUser && currentUser.uid}
       currentUser={currentUser}
+      primaryColor={primaryColor}
      />
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages
@@ -26,7 +30,7 @@ const App = ({currentUser, currentChannel, isPrivateChannel, userPosts}) => (
 
     <Grid.Column width={4}>
       <MetaPanel 
-        key={currentChannel && currentChannel.id}
+        key={currentChannel && currentChannel.name}
         currentChannel={currentChannel}
         isPrivateChannel={isPrivateChannel} 
         userPosts={userPosts}
@@ -39,7 +43,9 @@ const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
-  userPosts: state.channel.userPosts
+  userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor,
 })
 
 const mapDispatchToProps = {
